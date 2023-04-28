f48009e94cbd572f03b02a39217f537d41a22e30
Sufyan <Sufiik60@gmail.com>
2023-04-28 12:11:53 +0500

    feature: added javascript

--- index.js
+++ index.js
@@ -7,24 +7,39 @@ function Menu(){
   element.classList.toggle('active');
 }
 
-const addToCartButton = document.getElementById('add-to-cart');
-addToCartButton.addEventListener('click', addToCart);
-function addToCart() {
-  // Get the item details (e.g. name, price, quantity) from the page
-  const itemName = document.getElementById('item-name').textContent;
-  const itemPrice = parseFloat(document.getElementById('item-price').textContent);
-  const itemQuantity = parseInt(document.getElementById('item-quantity').value);
+// Select all "Add To Cart" buttons
+const addToCartButtons = document.querySelectorAll(".cart-btn");
 
-  // Add the item to the cart or update the quantity if it already exists
-  const cart = JSON.parse(localStorage.getItem('cart')) || {};
-  if (cart[itemName]) {
-    cart[itemName].quantity += itemQuantity;
-  } else {
-    cart[itemName] = { price: itemPrice, quantity: itemQuantity };
-  }
-  localStorage.setItem('cart', JSON.stringify(cart));
+// Add event listener to each button
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", addToCartClicked);
+});
 
-  // Update the cart icon or summary to reflect the new contents
-  updateCartIcon();
-  updateCartSummary();
+// Function to handle click event
+function addToCartClicked(event) {
+  // Get the button that was clicked
+  const button = event.target;
+
+  // Get the product that was clicked
+  const product = button.parentElement;
+
+  // Get the product information (name and price)
+  const productName = product.querySelector("h3").innerText;
+  const productPrice = product.querySelector("h6").innerText;
+
+  // Create a new cart item
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("cart-item");
+  cartItem.innerHTML = `
+    <span class="cart-item-name">${productName}</span>
+    <span class="cart-item-price">${productPrice}</span>
+    <button class="remove-btn">Remove</button>
+  `;
+
+  // Add the cart item to the cart
+  const cartItems = document.querySelector(".cart-items");
+  cartItems.appendChild(cartItem);
+
+  // Update the cart total
+  updateCartTotal();
 }
